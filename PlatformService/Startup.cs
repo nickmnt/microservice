d65b8759549360548aca762a95a26cd100bdc086
Nick <mtaghavinima@gmail.com>
2024-03-08 00:04:25 -0500

    Added automapper and profile for platform

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -32,6 +32,7 @@ namespace PlatformService
             services.AddScoped<IPlatformRepository, PlatformRepository>();
             
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
