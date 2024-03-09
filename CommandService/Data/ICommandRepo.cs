2cd9417904a5b92954e24a11d464bc3a5a62a6fe
Nick <mtaghavinima@gmail.com>
2024-03-08 19:21:12 -0500

    Added interface for CommandService repository

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using CommandService.Models;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+        
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlatformExists();
+        
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformIdx, Command command);
+    }
+}
