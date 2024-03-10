69cf24b0abd37849cd6004ca2e1f41d05ecfac5b
Nick <mtaghavinima@gmail.com>
2024-03-10 18:30:26 -0400

    Added grpc seed for Command Service

---
+++ CommandService/Data/Seed.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using CommandService.Models;
+using CommandService.SyncDataServices.Grpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandService.Data
+{
+    public static class Seed
+    {
+        public static void PrepPopulation(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var grpcClient = serviceScope.ServiceProvider.GetService<IPlatformDataClient>();
+
+                var platforms = grpcClient.ReturnAllPlatforms();
+                
+                SeedData(serviceScope.ServiceProvider.GetService<ICommandRepo>(), platforms);
+            }
+        }
+
+        private static void SeedData(ICommandRepo repo, IEnumerable<Platform> platforms)
+        {
+            Console.Out.WriteLine("Seeding new platforms...");
+
+            foreach (var plat in platforms)
+            {
+                if (!repo.ExternalPlatformExists(plat.ExternalId))
+                {
+                    repo.CreatePlatform(plat);
+                }
+
+                repo.SaveChanges();
+            } 
+        }
+    }
+}
--- CommandService/Startup.cs
+++ CommandService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using CommandService.AsyncDataServices;
 using CommandService.Data;
 using CommandService.EventProcessing;
+using CommandService.SyncDataServices.Grpc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,7 @@ namespace CommandService
             
             services.AddSingleton<IEventProcessor, EventProcessor>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IPlatformDataClient, PlatformDataClient>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandService", Version = "v1" });
@@ -60,6 +62,8 @@ namespace CommandService
             {
                 endpoints.MapControllers();
             });
+            
+            Seed.PrepPopulation(app);
         }
     }
 }
