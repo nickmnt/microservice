e86d2c05830f1fd04d84051de6943ed7d264ad7e
Nick <mtaghavinima@gmail.com>
2024-03-10 12:49:34 -0400

    Added GrpcPlatformService

---
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using Grpc.Core;
+using PlatformService.Data;
+
+namespace PlatformService.SyncDataServices.Grpc
+{
+    public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformService(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override Task<PlatformResponse> GetAllPlatforms(GetAllRequests request, ServerCallContext context)
+        {
+            var response = new PlatformResponse();
+            var platforms = _repository.GetAllPlatforms();
+
+            foreach (var platform in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(platform));
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
