20f4cdc26049847ac69c2f7b4a48ceaa730955bc
Nick <mtaghavinima@gmail.com>
2024-03-10 18:29:46 -0400

    Added Automapper mapping for GrpcPlatformModel

--- CommandService/Profiles/CommandsProfile.cs
+++ CommandService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandService.Dtos;
 using CommandService.Models;
+using PlatformService;
 
 namespace CommandService.Profiles
 {
@@ -15,6 +16,16 @@ namespace CommandService.Profiles
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalId,
                     opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalId,
+                    opt =>
+                        opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name,
+                    opt =>
+                        opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands,
+                    opt =>
+                        opt.Ignore());
         }
     }
 }
